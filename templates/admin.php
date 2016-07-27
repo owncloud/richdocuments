41d49ca2b2e697f4a820997bc337d3e105325853
Jan-Christoph Borchardt <hey@jancborchardt.net>
2016-07-27 12:27:39 +0200

    change naming to Office for real

--- templates/admin.php
+++ templates/admin.php
@@ -2,7 +2,7 @@
 script('richdocuments', 'admin');
 ?>
 <div class="section" id="richdocuments">
-	<h2><?php p($l->t('Collabora Online')) ?></h2>
+	<h2><?php p($l->t('Office')) ?></h2>
 	<label for="wopi_url"><?php p($l->t('Collabora Online server')) ?></label>
 	<input type="text" name="wopi_url" id="wopi_url" value="<?php p($_['wopi_url'])?>" style="width:300px;">
 	<br /><em><?php p($l->t('URL (and port) of the Collabora Online server that provides the editing functionality as a WOPI client.')) ?></em>
