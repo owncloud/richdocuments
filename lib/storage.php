fafe9658782fb09809cc0630043928e2159756ed
Andras Timar <andras.timar@collabora.com>
2017-01-07 21:16:22 +0100

    add rtf and txt as supported file formats

--- lib/storage.php
+++ lib/storage.php
@@ -38,6 +38,7 @@ class Storage {
 		'application/msword',
 		'application/rtf',
 		'text/rtf',
+		'text/plain',
 		'application/vnd.openxmlformats-officedocument.wordprocessingml.document',
 		'application/vnd.openxmlformats-officedocument.wordprocessingml.template',
 		'application/vnd.ms-word.document.macroEnabled.12',
