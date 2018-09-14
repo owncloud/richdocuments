72d51245ce022c54cf26945523d7f11c926de5ac
ownClouders <devops@owncloud.com>
2018-09-14 05:01:09 +0000

    [tx] updated from transifex

--- l10n/it.js
+++ l10n/it.js
@@ -37,6 +37,7 @@ OC.L10N.register(
     "Just now" : "In questo istante",
     "Failed to revert the document to older version" : "Il ripristino del documento alla versione più datata non è riuscito",
     "Save As" : "Salva come",
+    "New filename" : "Nuovo nome file",
     "Cancel" : "Annulla",
     "Save" : "Salva",
     "No connection to server. Trying to reconnect." : "Nessuna connessione al server. Riprova a connetterti.",
