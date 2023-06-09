2c7b53f62f2d0874127c219467ae8496d82d0ac4
Benedikt Kulmann <benedikt@kulmann.biz>
2023-06-09 14:46:23 +0200

    feat: upgrade to vue3, pnpm, @ownclouders/extension-sdk

---
+++ src/index.ts
@@ -0,0 +1,86 @@
+import editor from './editor.vue'
+
+const routes = [
+  {
+    path: '/:driveAliasAndItem(.*)?',
+    components: editor,
+    name: 'editor',
+    meta: {
+      title: 'Collabora Online',
+      patchCleanPath: true
+    }
+  }
+]
+
+const appInfo = {
+  name: 'Collabora Online',
+  id: 'richdocuments',
+  icon: 'resource-type-text',
+  iconFillType: 'fill',
+  extensions: [
+    {
+      extension: 'odt',
+      routeName: 'richdocuments-editor',
+      newFileMenu: {
+        menuTitle($gettext) {
+          return $gettext('Document')
+        },
+        icon: 'x-office-document'
+      },
+      canBeDefault: true
+    },
+    {
+      extension: 'docx',
+      routeName: 'richdocuments-editor',
+      canBeDefault: true
+    },
+    {
+      extension: 'ods',
+      routeName: 'richdocuments-editor',
+      newFileMenu: {
+        menuTitle($gettext) {
+          return $gettext('Spreadsheet')
+        },
+        icon: 'x-office-spreadsheet'
+      },
+      canBeDefault: true
+    },
+    {
+      extension: 'xlsx',
+      routeName: 'richdocuments-editor',
+      canBeDefault: true
+    },
+    {
+      extension: 'odp',
+      routeName: 'richdocuments-editor',
+      newFileMenu: {
+        menuTitle($gettext) {
+          return $gettext('Presentation')
+        },
+        icon: 'x-office-presentation'
+      },
+      canBeDefault: true
+    },
+    {
+      extension: 'pptx',
+      routeName: 'richdocuments-editor',
+      canBeDefault: true
+    },
+    {
+      extension: 'odg',
+      routeName: 'richdocuments-editor',
+      newFileMenu: {
+        menuTitle($gettext) {
+          return $gettext('Drawing')
+        },
+        icon: 'x-office-drawing'
+      },
+      canBeDefault: true
+    }
+  ]
+}
+
+export default {
+  appInfo,
+  routes
+}
